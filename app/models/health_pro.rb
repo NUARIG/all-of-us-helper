3a959fe69aef6ca7cedea4c1e08da0fbb76dda75
Michael Gurley <m-gurley@northwestern.edu>
2022-06-06 07:21:16 -0400

    Fix SQL.

--- app/models/health_pro.rb
+++ app/models/health_pro.rb
@@ -159,9 +159,17 @@ class HealthPro < ApplicationRecord
     p
   end
 
-  scope :by_biospecimens_location, ->(biospecimens_location) do
+  scope :by_biospecimens_location_declined, ->(biospecimens_location) do
     if !['all'].include?(biospecimens_location)
-      p = where('EXISTS (SELECT 1 FROM health_pros hp2 WHERE health_pros.pmi_id = hp2.pmi_id AND hp2.biospecimens_location = ? AND hp2.id = (SELECT max(hp3.id) FROM health_pros hp3 WHERE hp2.pmi_id = hp3.pmi_id))', biospecimens_location)
+      p = joins("JOIN (SELECT hp2.pmi_id, max(id) as id
+                 FROM health_pros hp2
+                 WHERE pmi_id in(
+                                  SELECT pmi_id
+                                  FROM health_pros hp 3
+                                  WHERE hp3.status = 'declined'
+                                )
+                 GROUP BY hp2.pmi_id
+                 ) hp4 ON health_pros.pmi_id = hp4.pmi_id JOIN health_pros hp5 ON hp4.id = hp5.id").where('hp5.biospecimens_location = ?', biospecimens_location)
     else
       p =  all
     end
