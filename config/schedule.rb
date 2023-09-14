bbb17570af5b91c75ef6ec155bd8e2931c014186
Yulia A Bushmanova <y-bushmanova@northwestern.edu>
2023-09-14 13:48:24 -0500

    Temporarily disable scheduled tasks on production. #243

--- config/schedule.rb
+++ config/schedule.rb
@@ -24,40 +24,40 @@ set :output, {:error => 'log/whenever_error.log', :standard => 'log/whenever.log
 
 case environment
   when 'production'
-    every :wednesday, at: '2:30pm' do # Use any day of the week or :weekend, :weekday
-      rake "recruitment:load_export_and_cohorts"
-    end
-
-    every 1.day, at: ['10:00 am', '4:00 pm'] do
-      rake "recruitment:load_cohorts"
-    end
-
-    every :day, at: '12:00am' do # Use any day of the week or :weekend, :weekday
-      rake "health_pro_api:rotate_service_account_key"
-    end
-
-    every :day, at: '12:15am' do # Use any day of the week or :weekend, :weekday
-      rake "maintenance:expire_batch_health_pros"
-    end
-
-    every :day, at: '12:30am' do # Use any day of the week or :weekend, :weekday
-      rake "health_pro_api:import_api"
-    end
-
-    every :monday, at: '8:00am' do # Use any day of the week or :weekend, :weekday
-      rake "health_pro_api:export_api_data"
-    end
-
-    every 1.day, at: ['9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm'] do
-      rake "redcap:synch_patients"
-    end
-
-    every :day, at: '7:00 pm' do # Use any day of the week or :weekend, :weekday
-      rake "redcap:synch_deleted_patients"
-    end
-
-    every 1.day, at: ['8:00 am', '2:00 pm'] do
-      rake "redcap:synch_patients_to_redcap"
-    end
+    # every :wednesday, at: '2:30pm' do # Use any day of the week or :weekend, :weekday
+    #   rake "recruitment:load_export_and_cohorts"
+    # end
+    #
+    # every 1.day, at: ['10:00 am', '4:00 pm'] do
+    #   rake "recruitment:load_cohorts"
+    # end
+    #
+    # every :day, at: '12:00am' do # Use any day of the week or :weekend, :weekday
+    #   rake "health_pro_api:rotate_service_account_key"
+    # end
+    #
+    # every :day, at: '12:15am' do # Use any day of the week or :weekend, :weekday
+    #   rake "maintenance:expire_batch_health_pros"
+    # end
+    #
+    # every :day, at: '12:30am' do # Use any day of the week or :weekend, :weekday
+    #   rake "health_pro_api:import_api"
+    # end
+    #
+    # every :monday, at: '8:00am' do # Use any day of the week or :weekend, :weekday
+    #   rake "health_pro_api:export_api_data"
+    # end
+    #
+    # every 1.day, at: ['9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm'] do
+    #   rake "redcap:synch_patients"
+    # end
+    #
+    # every :day, at: '7:00 pm' do # Use any day of the week or :weekend, :weekday
+    #   rake "redcap:synch_deleted_patients"
+    # end
+    #
+    # every 1.day, at: ['8:00 am', '2:00 pm'] do
+    #   rake "redcap:synch_patients_to_redcap"
+    # end
   when 'staging'
 end
