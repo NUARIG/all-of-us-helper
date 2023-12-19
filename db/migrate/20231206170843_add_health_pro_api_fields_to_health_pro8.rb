ef9eadd468d7cebd306a0cfd6e20e04314f339c4
Yulia A Bushmanova <y-bushmanova@northwestern.edu>
2023-12-19 00:02:35 -0600

    Hotfix: attempt to fix long running migration.

--- db/migrate/20231206170843_add_health_pro_api_fields_to_health_pro8.rb
+++ db/migrate/20231206170843_add_health_pro_api_fields_to_health_pro8.rb
@@ -1,8 +1,8 @@
 class AddHealthProApiFieldsToHealthPro8 < ActiveRecord::Migration[5.1]
-  def change
-    add_column :health_pros, :questionnaire_on_behaviorial_health_and_personality_authored, :string
-    add_column :health_pros, :questionnaire_on_emotional_health_history_and_well_being_authored, :string
-    add_column :health_pros, :questionnaire_on_environmental_exposures, :string
-    add_column :health_pros, :questionnaire_on_environmental_exposures_authored, :string
+  change_table(:health_pros, bulk: true) do |t|
+    t.column   :questionnaire_on_behaviorial_health_and_personality_authored, :string
+    t.column   :questionnaire_on_emotional_health_history_and_well_being_authored, :string
+    t.column   :questionnaire_on_environmental_exposures, :string
+    t.column   :questionnaire_on_environmental_exposures_authored, :string
   end
 end
